911cf37be84d5cecd862f5440466a94ac31c0012
17205976 <min.long@ucdconnect.ie>
2020-03-19 17:18:59 +0800

    code update

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Group-6-Animal-Hospital-System
+
+## Introduction:
+
+This is an online system for a animal hospital called Healing  paws Veterinary Hospital.
+
+
+
 Group Member|UCD number
 --|:--:
 Yu Jiayue|17206173
@@ -7,4 +14,6 @@ Long Min|17205976
 Liu Haoran|17205986
 Xu Ziting|16206791
 Zheng Zhi|17206148
+
+
 
---
+++ web code (for windows)/blogapp/routes.py
@@ -0,0 +1,77 @@
+from flask import render_template, flash, redirect, url_for, session, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from blogapp import app, db
+from blogapp.forms import LoginForm, SignupForm
+from blogapp.models import User
+from blogapp.config import Config
+import os
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'User'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user_in_db = User.query.filter(User.username == form.username.data).first()
+        if not user_in_db:
+            flash('No user found with username: {}'.format(form.username.data))
+            return redirect(url_for('login'))
+        if (check_password_hash(user_in_db.password_hash, form.password.data)):
+            flash('Login success!')
+            session["USERNAME"] = user_in_db.username
+            return redirect(url_for('login'))
+        # should go to the logged-in page
+        flash('Incorrect Password')
+        return redirect(url_for('login'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password2.data:
+            flash('Passwords do not match!')
+            return redirect(url_for('signup'))
+        user_in_db = User.query.filter(User.username == form.username.data).first()
+        if user_in_db:
+            flash('Username already existed!')
+            return redirect(url_for('signup'))
+        passw_hash = generate_password_hash(form.password.data)
+        user = User(username=form.username.data, email=form.email.data, password_hash=passw_hash)
+        db.session.add(user)
+        db.session.commit()
+        flash('User registered with username:{}'.format(form.username.data))
+        session["USERNAME"] = user.username
+        return redirect(url_for('login'))
+    return render_template('signup.html', title='Register a new user', form=form)
+
+
+@app.route('/checkuser', methods=['POST'])
+def check_username():
+    chosen_name = request.form['username']
+    user_in_db = User.query.filter(User.username == chosen_name).first()
+    if not user_in_db:
+        return jsonify({'text': 'Username is available',
+                        'returnvalue': 0})
+    else:
+        return jsonify({'text': 'Sorry! Username is already taken',
+                        'returvalue': 1})
+
+
+@app.route('/checkemail', methods=['POST'])
+def check_email():
+    chosen_email = request.form['email']
+    user_in_db = User.query.filter(User.email == chosen_email).first()
+    if not user_in_db:
+        return jsonify({'text': 'Email is available',
+                        'returnvalue': 0})
+    else:
+        return jsonify({'text': 'Sorry! Email is already taken',
+                        'returvalue': 1})
